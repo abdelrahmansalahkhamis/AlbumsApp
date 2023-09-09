f1017a12791070d947d9ebbf8a41c53e658e94db
Abdelrahman Salah <a.salah@msd-my.com>
2023-09-09 19:34:51 +0300

    Initial Commit

---
+++ AlbumsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumsApp
+//
+//  Created by Abdelrahman Salah on 09/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
