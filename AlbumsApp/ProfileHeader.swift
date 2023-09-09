f2692d4368dec91acdee7b37f17a79d78dab50f8
Abdelrahman Salah <a.salah@msd-my.com>
2023-09-10 00:46:51 +0300

    implement profile screen

---
+++ AlbumsApp/ProfileHeader.swift
@@ -0,0 +1,59 @@
+//
+//  ProfileHeader.swift
+//  AlbumsApp
+//
+//  Created by Abdelrahman Salah on 10/09/2023.
+//
+
+import UIKit
+
+class ProfileHeader: UITableViewHeaderFooterView {
+
+    func setUpUI(){
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = 10 // Adjust spacing as needed
+
+        let profileLbl = UILabel()
+        profileLbl.text = "Profile"
+        profileLbl.textAlignment = .left
+        profileLbl.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+
+
+        let username = UILabel()
+        username.text = "Leanne Graham"
+        username.textAlignment = .left
+        username.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+
+
+        let userAddress = UILabel()
+        userAddress.text = "Apt. 556, Gwenborough, 92998-3874"
+        userAddress.textAlignment = .left
+        userAddress.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+
+
+        let myProfileLbl = UILabel()
+        myProfileLbl.text = "My Profile"
+        myProfileLbl.textAlignment = .center
+        myProfileLbl.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+
+        stackView.addArrangedSubview(profileLbl)
+        stackView.addArrangedSubview(username)
+        stackView.addArrangedSubview(userAddress)
+        stackView.addArrangedSubview(myProfileLbl)
+
+        self.addSubview(stackView)
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        // Center the stack view horizontally and vertically
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
+
+}
+
